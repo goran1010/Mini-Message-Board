812f96ec1a4e5a6e1378846f4f183550a2a78f20
Goran Jovic <goran1010jovic@gmail.com>
2025-05-31 14:12:55 +0200

    Changed db connections

--- models/pool.js
+++ models/pool.js
@@ -2,9 +2,11 @@ const { Pool } = require("pg");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const ROLENAME = process.env.ROLENAME;
-const ROLEPASSWORD = process.env.ROLEPASSWORD;
+const DATABASE_HOST = process.env.DATABASE_HOST;
+const DATABASE_USER = process.env.DATABASE_USER;
+const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD;
+const DATABASE_NAME = process.env.DATABASE_NAME;
 
 module.exports = new Pool({
-  connectionString: `postgresql://${ROLENAME}:${ROLEPASSWORD}@localhost:5432/messages`,
+  connectionString: `postgresql://${DATABASE_USER}:${DATABASE_PASSWORD}@${DATABASE_HOST}/${DATABASE_NAME}`,
 });
